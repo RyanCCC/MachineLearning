8f69b1fc236fdc3b55724aecf0d209b4338e047f
ryanccc <chenjialiang666@163.com>
2019-11-03 21:31:25 +0800

    matplotlib demo

--- DataAnalysising/MatplotlibDemo.py
+++ DataAnalysising/MatplotlibDemo.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    print('hello world')
+    # print('hello world')
+    fig = plt.figure(figsize=(20, 8), dpi = 80)
     x = range(2, 26, 2)
     y = [15, 13, 14, 5, 17, 20, 25, 26, 26, 24, 22, 18]
     plt.plot(x, y)
+    plt.savefig("./sig_size.png")
     plt.show()
