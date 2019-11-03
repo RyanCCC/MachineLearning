bb637c45f085ed085b3e70feeff4b878aa40f25c
ryanccc <chenjialiang666@163.com>
2019-11-03 22:50:25 +0800

    demo

--- DataAnalysising/MatplotlibDemo.py
+++ DataAnalysising/MatplotlibDemo.py
@@ -1,4 +1,16 @@
 from matplotlib import pyplot as plt
+import matplotlib
+from matplotlib import font_manager
+
+# font= {
+#     'family':'MicroSoft YaHei',
+#     'weight':'bold',
+#     'size':'larger'
+# }
+
+# matplotlib.rc("font", **font)
+
+# my_font = font_manager.FontProperties()
 
 if __name__ == '__main__':
     # print('hello world')
@@ -6,5 +18,13 @@ if __name__ == '__main__':
     x = range(2, 26, 2)
     y = [15, 13, 14, 5, 17, 20, 25, 26, 26, 24, 22, 18]
     plt.plot(x, y)
+    # 设置x刻度
+    _x = x
+    _xticks_labels = [f"10:0{i+1}" if i<9 else f"10:{i+1}" for i in range(0,13)]
+    plt.xticks(x, _xticks_labels,rotation=90)
+    plt.yticks(range(min(y),max(y)+1))
+    plt.xlabel("Time")
+    plt.ylabel("Tempearature")
+    plt.title('Temperature')
     plt.savefig("./sig_size.png")
     plt.show()
