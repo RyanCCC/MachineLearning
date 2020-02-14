57714694c45902bcb7ab0247de34cef79b66bb96
ryanccc <chenjialiang666@.com>
2020-02-14 14:50:53 +0800

    Test

--- Mian.py
+++ Mian.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
+
+# 递归统计一个1-9999中出现的数字
+def count_digit(number):
+    return len(str(number))
+
+def countThree(digit):
+    if not isinstance(digit,int):
+        raise TypeError('number is not int')
+    # digit = len(str(number))
+    if(digit <=0):
+        return 0
+    if(digit ==1):
+        return 1
+    return 10*countThree(digit-1) + 10 **(digit-1)
 if __name__ =='__main__':
+    # print(10**2)
+    print(countThree(count_digit(9999)))
     labels='商城批发','超市','专营店','网络'
 
     sizes=55,8,12,25
